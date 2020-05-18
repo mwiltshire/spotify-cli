3ec58789f5ac1337cf914a6b7b4c8fd42bb4847b
Mark Wiltshire <mark.wiltshire88@gmail.com>
2020-05-18 22:32:15 +0200

    mirror conf interface to handle get error

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,16 @@
 import Conf from 'conf';
 
-export default new Conf();
+const config = new Conf();
+
+export default {
+  get(key: string) {
+    const value = config.get(key);
+    if (!value) {
+      throw new Error(`Unknown config key: ${key}`);
+    }
+    return value;
+  },
+  set(key: string, value: any) {
+    config.set(key, value);
+  }
+};
