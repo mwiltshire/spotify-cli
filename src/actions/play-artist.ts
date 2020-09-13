34adf075b6422b381fb489fe8d4ffb67d6edc0b9
Mark Wiltshire <mark.wiltshire88@gmail.com>
2020-09-13 15:10:28 +0200

    remove debugging

--- src/actions/play-artist.ts
+++ src/actions/play-artist.ts
@@ -3,12 +3,11 @@ import injectSpotify from '../utils/injectSpotify';
 import logger from '../utils/logger';
 
 const action = async (spotify: SpotifyWebApi, args: [any, string[]]) => {
-  const [cmd, [query]] = args;
-  console.log(cmd, query);
-  // const res = await spotify.searchArtists(query);
-  // await spotify.play({
-  //   context_uri: res.body.artists?.items[0].uri as string
-  // });
+  const [, [query]] = args;
+  const res = await spotify.searchArtists(query);
+  await spotify.play({
+    context_uri: res.body.artists?.items[0].uri as string
+  });
 };
 
 const playArtist = async (...args: string[]) => {
